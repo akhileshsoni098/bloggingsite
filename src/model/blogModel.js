b6356a753f42d83598ee3a21e97d3c291f6aabbf
Tapaswee2001Samantaray <tapas10231@gmail.com>
2023-01-02 16:00:49 +0530

    blog

---
+++ src/model/blogModel.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+const objectId = mongoose.Schema.Types.ObjectId;
+
+const blogSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    body: {
+        type: String,
+        require: true
+    },
+    authorId: {
+        type: objectId,
+        ref: "authorData"
+    },
+    tags: [String],
+    category: {
+        type: String,
+        required: true
+    },
+    subcategory: mongoose.Schema.Types.Mixed,
+    deletedAt: Date.now(),
+    isDeleted: {
+        type: Boolean,
+        default: false
+    },
+    publishedAt: Date.now(),
+    isPublished: {
+        type: Boolean,
+        default: false
+    }
+}, { timestamps: true });
+
+module.exports = mongoose.model("Blog", blogSchema);
