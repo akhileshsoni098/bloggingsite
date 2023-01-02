2811b5cddd710368ba1e0c54fd2def23f87c5926
Akhilesh Soni <akhileshsoni098@gmail.com>
2023-01-02 16:14:51 +0530

    author

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,7 +2,7 @@
 
 const express = require('express')
 const router = express.Router()
-const AuthorData = require('../controller/authorContro')
+const AuthorData = require('../controller/authorController')
 
 
 
